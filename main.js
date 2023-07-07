8d55a592220d85bbb6afed54d403f280e67c266e
bhargav <bhargav.annem@gmail.com>
2023-07-07 00:50:44 -0500

    feat: cube world

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const gl = canvas.getContext("webgl");
 var datInput = {
   renderSmoothen: 1000,
   depth: 3.0,
-  Scale: 2.0,
+  Scale: 8.0,
   Power: 1.0,
   Offset: 1.0,
   Rotation: 1.0,
+  minRadius2: 1.0,
+  fixedRadius2: 1.0,
+  foldingLimit: 1.0,
 };
 var gui;
 function main() {
@@ -31,13 +34,16 @@ function main() {
     uniform float     Power;
     uniform float     Rotation;
     uniform float     Offset;
+    uniform float     minRadius2;
+    uniform float     fixedRadius2;
+    uniform float     foldingLimit;
     uniform vec4      iMouse;
     precision highp float;
 
-    #define MAXSTEPS 35
-    #define z_near 10e-6
-    #define z_far 3.5
-    #define Iterations 20 
+    #define MAXSTEPS 10
+    #define z_near 10e-4
+    #define z_far 2.5
+    #define Iterations 10 
     // ########################### STRUCTS #############################
 
     
@@ -48,29 +54,37 @@ function main() {
 
     // ########################## MAIN        ##########################
 
+    void sphereFold(inout vec3 z, inout float dz) {
+      float r = length(z);
+	float r2 = dot(z,z);
+	if (r<minRadius2) { 
+		float temp = (fixedRadius2/minRadius2);
+		z *= temp;
+		dz*= temp;
+	} else if (r2<fixedRadius2) { 
+		float temp =(fixedRadius2/r2);
+		z *= temp;
+		dz*= temp;
+	}
+}
+
+void boxFold(inout vec3 z, inout float dz) {
+	z = clamp(z, -foldingLimit, foldingLimit) * 2.0 - z;
+}
+
     float DE(vec3 pos) {
 vec3 z = pos;
+  	vec3 offset = z;
 	float dr = 1.0;
-	float r = 0.0;
-	for (int i = 0; i < Iterations ; i++) {
-		r = length(z);
-		if (r>z_far) break;
-		
-		// convert to polar coordinates
-		float theta = acos(z.z/r);
-		float phi = atan(z.y,z.x);
-		dr =  pow( r, Power-1.0)*Power*dr + 1.0;
-		
-		// scale and rotate the point
-		float zr = pow( r,Power);
-		theta = theta*Power;
-		phi = phi*Power;
-		
-		// convert back to cartesian coordinates
-		z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
-		z+=pos;
+	for (int n = 0; n < Iterations; n++) {
+		boxFold(z,dr);       // Reflect
+		sphereFold(z,dr);    // Sphere Inversion
+ 		
+                z=Scale*z + offset;  // Scale & Translate
+                dr = dr*abs(Scale)+1.0;
 	}
-	return 0.5*log(r)*r/dr;
+	float r = length(z);
+	return r/abs(dr);
     }
     
     vec3 ray_color(Ray r){
@@ -95,7 +109,7 @@ vec3 z = pos;
       uv.x *= aspect_ratio;
       
       vec3 target = vec3(0.0);
-      vec3 origin = vec3(depth * sin(Rotation), 1.0, -depth * cos(Rotation));
+      vec3 origin = vec3(depth * sin(Rotation), 0.0, -depth * cos(Rotation));
       vec3 dir = normalize(vec3(uv, 1.0));
 
       dir.xz *= mat2(cos(Rotation), -sin(Rotation), sin(Rotation), cos(Rotation));
@@ -124,6 +138,9 @@ vec3 z = pos;
   var power = gl.getUniformLocation(program, "Power");
   var offset = gl.getUniformLocation(program, "Offset");
   var rotation = gl.getUniformLocation(program, "Rotation");
+  var fixedRadius2 = gl.getUniformLocation(program, "fixedRadius2");
+  var foldingLimit = gl.getUniformLocation(program, "foldingLimit");
+  var minRadius2 = gl.getUniformLocation(program, "minRadius2");
   var mouse = gl.getUniformLocation(program, "iMouse");
   function render(deltaMS) {
     gl.viewport(0, 0, canvas.width, canvas.height);
@@ -136,6 +153,9 @@ vec3 z = pos;
     gl.uniform1f(power, datInput.Power);
     gl.uniform1f(offset, datInput.Offset);
     gl.uniform1f(rotation, datInput.Rotation);
+    gl.uniform1f(fixedRadius2, datInput.fixedRadius2);
+    gl.uniform1f(foldingLimit, datInput.foldingLimit);
+    gl.uniform1f(minRadius2, datInput.minRadius2);
     gl.uniform4fv(mouse, [mousepos[0], mousepos[1], 0, 0]);
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -154,8 +174,11 @@ function initializeGUI() {
   var inputFolder = g.addFolder("Input");
   inputFolder.add(datInput, "renderSmoothen", 1, 1000);
   inputFolder.add(datInput, "depth", 0, 5);
-  inputFolder.add(datInput, "Scale", 0, 5);
+  inputFolder.add(datInput, "Scale", 8, 15);
   inputFolder.add(datInput, "Power", 0, 15);
   inputFolder.add(datInput, "Offset", 0, 5);
+  inputFolder.add(datInput, "foldingLimit", 0, 25);
+  inputFolder.add(datInput, "fixedRadius2", 0, 25);
+  inputFolder.add(datInput, "minRadius2", 0, 25);
   inputFolder.add(datInput, "Rotation", 0, 2.0 * Math.PI);
 }
